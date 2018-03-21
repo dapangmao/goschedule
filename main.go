b91babccb3dc28d78ebc5584e1a66ceda1f12df9
dapangmao <hchao8@gmail.com>
2018-03-21 19:44:17 -0400

    clean the wrong merge

--- main.go
+++ main.go
@@ -1,28 +1,9 @@
 package main
 
-<<<<<<< HEAD
 
 
 
 func main() {
 
 
-=======
-import "fmt"
-
-
-type test struct {
-	a int
-	b int
-}
-
-func (t *test) sum() int {
-	return t.a + t.b
-}
-
-
-func main() {
-	a := &test{1, 2}
-	fmt.Println(a.sum())
->>>>>>> f55b5065742085b2978640078bbe76342b8994e9
 }
