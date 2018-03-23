da4807274aad2cdff0fb9799485ed25602d54e98
dapangmao <hchao8@gmail.com>
2018-03-23 16:06:42 -0400

    add travis yml

--- webui.go
+++ webui.go
@@ -7,13 +7,6 @@ import (
 	"strconv"
 )
 
-//func helloHandle(w http.ResponseWriter, r *http.Request) {
-//	key, ok := r.URL.Query()["key"]
-//	if ok {
-//		ui2sched  <- strings.Join(key, "")
-//	}
-//	fmt.Fprintf(w, "Hello World, you reached %s \n", r.URL.Path)
-//}
 
 
 func console(w http.ResponseWriter, r *http.Request) {
